6e03c52e907cacf8cb03bef0192b000ef12354e3
root <root@David.01xjoj1tsyoerarvp5in54m2pa.bx.internal.cloudapp.net>
2021-07-09 13:08:23 +0000

    added some notes

--- app.py
+++ app.py
@@ -3,24 +3,33 @@ import json
 import csv
 
 def main():
+
+    # get first page of characters and the amount of pages needed from the API
     url = 'https://rickandmortyapi.com/api/character/?species=human&status=alive'
     req = requests.get(url)
+    # load json object from the response
     json_obj = json.loads(req.content)
-
+    # set pages range so we'll be able to loop over them 
     pages = range(1, int(json_obj['info']['pages']+1))
 
+    # open and create the csv file
     csv_file = open("results.csv", "w") 
+    # define the csv headers
     data = [['Name', 'Location', 'Image']]
-
+    
+    # loop over the pages and look for characters from earth
     for page in pages:
         for char in json_obj['results']:
+            # if character is from earth, add it to data
             if 'Earth' in char['origin']['name']:
                 data.append([char['name'], char['location']['name'], char['image']])
-                #csv.writer(csv_file).writerow(data)
+        
+        # if its not the last page, get the next page and load it as json
+        if page != pages[-1]:
+            req = requests.get(url+"&page="+str(page+1))
+            json_obj = json.loads(req.content)
 
-        req = requests.get(url+"&page="+str(page+1))
-        json_obj = json.loads(req.content)
-
+    # write all the data to the csv file and close it
     csv.writer(csv_file).writerows(data)
     csv_file.close()
 
